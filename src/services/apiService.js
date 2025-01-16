f96fb87d86163c8d3c97305cdc27660eb8bae696
princekumar205086 <prince_kumar_mca21@jimsindia.org>
2025-01-17 02:55:28 +0530

    app url fixed

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/login/";
+const API_URL = "http://https://ptpi.tech/api/login/";
 
 const logInservice = async (email, password) => {
   const formData = new FormData();
--- src/store/configue.js
+++ src/store/configue.js
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl : import.meta.env.VITE_API_URL || 'teacher-hire-frontend/.env',
-    pinCodeUrl : import.meta.env.VITE_POSTAL_API_URL || 'teacher-hire-frontend/.env',
+    apiUrl : import.meta.env.VITE_API_URL || 'https://ptpi.tech/',
+    pinCodeUrl : import.meta.env.VITE_POSTAL_API_URL || 'https://ptpi.tech/',
     };
   
    
