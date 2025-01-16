942ce3a02cad6117118d935b488e1dd09f5adc70
princekumar205086 <prince_kumar_mca21@jimsindia.org>
2025-01-17 02:56:20 +0530

    app url fixed

--- src/store/configue.js
+++ src/store/configue.js
@@ -1,6 +1,6 @@
 const config = {
     apiUrl : import.meta.env.VITE_API_URL || 'https://ptpi.tech/',
-    pinCodeUrl : import.meta.env.VITE_POSTAL_API_URL || 'https://ptpi.tech/',
+    pinCodeUrl : import.meta.env.VITE_POSTAL_API_URL || 'https://api.postalpincode.in/pincode/',
     };
   
    
